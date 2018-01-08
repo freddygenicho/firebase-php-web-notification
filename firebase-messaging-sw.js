7c4caa6e6bd26e87841e5cfff39a8814feb9f796
Fredrick Ochieng <fredygenicho@gmail.com>
2018-01-08 09:24:09 +0300

    implemented firebase notifications

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,31 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+var config = {
+    apiKey: "AIzaSyDls2l1t2y1UMU9bKQpdZEoNstNJYmY8sU",
+    authDomain: "fire-chat-93370.firebaseapp.com",
+    databaseURL: "https://fire-chat-93370.firebaseio.com",
+    projectId: "fire-chat-93370",
+    storageBucket: "fire-chat-93370.appspot.com",
+    messagingSenderId: "168789917062"
+};
+
+firebase.initializeApp(config);
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    const title = payload.data.title;
+    const options = {
+        body: payload.data.body
+    };
+
+    return self.registration.showNotification(title, options)
+});
--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 var config = {
-    apiKey: "your api key",
-    authDomain: "your auth domain",
-    databaseURL: "your database url",
-    storageBucket: "your storage bucket",
-    messagingSenderId: "your messaging id"
+    apiKey: "AIzaSyDls2l1t2y1UMU9bKQpdZEoNstNJYmY8sU",
+    authDomain: "fire-chat-93370.firebaseapp.com",
+    databaseURL: "https://fire-chat-93370.firebaseio.com",
+    projectId: "fire-chat-93370",
+    storageBucket: "fire-chat-93370.appspot.com",
+    messagingSenderId: "168789917062"
 };
+
 firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
+
+messaging.requestPermission()
+    .then(function () {
+        console.log('Notification permission granted.');
+        return messaging.getToken();
+    })
+    .then(function (token) {
+        //todo send token to server
+        console.log(token); // Display user token
+    })
+    .catch(function (err) {
+        //happens when user denies the permission
+        console.log('Unable to get permission to notify.', err);
+    });
+
+messaging.onMessage(function (payload) {
+    console.log('onMessage', payload);
+    alert(payload.notification.title)
+});
