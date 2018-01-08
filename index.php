1beccf83026a94a2d2857aba65c3f00c79354b4e
Fredrick Ochieng <fredygenicho@gmail.com>
2018-01-08 07:04:03 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Firebase Web Notification</title>
+</head>
+<body>
+<script src="https://www.gstatic.com/firebasejs/3.7.2/firebase.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "your api key",
+    authDomain: "your auth domain",
+    databaseURL: "your database url",
+    storageBucket: "your storage bucket",
+    messagingSenderId: "your messaging id"
+};
+firebase.initializeApp(config);
